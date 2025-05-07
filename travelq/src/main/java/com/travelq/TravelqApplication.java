02eb7acd98422992d61e4498c71e2878471394cb
Andrei-Stefanel Murariu (iamxorum) <andrei.smurariu@gmail.com>
2025-05-07 22:04:52 +0300

    Prepare Project, Spring Initializr #2

---
+++ travelq/src/main/java/com/travelq/TravelqApplication.java
@@ -0,0 +1,13 @@
+package com.travelq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelqApplication.class, args);
+	}
+
+}
---
+++ travelq/src/test/java/com/travelq/TravelqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
